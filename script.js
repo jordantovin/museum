c3eddf9376be45e25ff880218ac1d0f0864401b7
jordantovin <jordantovin@gmail.com>
2026-01-26 16:59:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // Configuration
 const CONFIG = {
     csvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTKj5a8JIDfxvaw-5pPEb5nHfu_a-jZS9lFgrHqvv6JjzCTbpmMTyxVxqF5yrZPjkH961zi-u_HvQwz/pub?output=csv',
-    webAppUrl: 'https://script.google.com/macros/s/AKfycbwWunN7dhoswN3Q67kmhOFdT6Kj7UJtC6ACwT5CPpmp87DxR02ywssI8r6aJn7qFg4/exec',
+    webAppUrl: 'https://script.google.com/macros/s/AKfycbwM6nl5HraFn9uo0BCAGmqIadSC82Ke6_OUa0SzVcix7dpnZDQbGFOB2OLBVWeSjnc/exec',
     sheetNames: {
         object: 'Object',
         sticker: 'Sticker',
